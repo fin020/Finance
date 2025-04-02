5375245c985f00517c8b572aebbf655ac5fc9a7d
Finley Sutcliffe <140662850+fin020@users.noreply.github.com>
2025-04-02 15:09:37 +0000

    just now lol

--- Efficient_frontier.py
+++ Efficient_frontier.py
@@ -9,10 +9,10 @@ def getData(stocks, start, end):
     stockData = yf.download(stocks, start=start, end=end)
     return stockData
 
-List = ['AAPL', 'MSFT', 'ABC']
-Stock = [stock+'.AX' for stock in List]
+List = ['AAPL', 'MSFT', 'GOOG']
+# Stock = [stock+'.AX' for stock in List]
 
 endDate = dt.datetime.now()
 startDate = endDate - dt.timedelta(days=365)
 
-print(getData(Stock, start=startDate, end=endDate))
+print(getData(List, start=startDate, end=endDate))
